8bf95b2d06bd60ab8b859def959b347106fd4d6c
Luna Morningstar <stewartsarah1990@gmail.com>
2022-04-22 07:31:32 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = ({ props }) => (
+  <div>
+    Webpack HMR is Working! Hello!
+  </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const { webpack } = require('webpack')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
