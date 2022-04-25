9ad8682f4cb43115c2e2a12e4c73e09ecbed3fd6
Luna Morningstar <stewartsarah1990@gmail.com>
2022-04-24 22:44:45 -0600

    Removing node modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 
+
 // Opt-in to Webpack hot module replacement
 if (module.hot) module.hot.accept()
 
