02d1611ea0e6a2c41bda097fb424dcbd7613265b
yukijiru <stewartsarah1990@gmail.com>
2022-05-08 05:49:07 -0600

    learning about find by puid

---
+++ app.js
@@ -0,0 +1,11 @@
+const express  = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-route');
+
+
+const app = express();
+
+app.use('/api/places', placesRoutes); // => /api/places...
+
+app.listen(9000);
